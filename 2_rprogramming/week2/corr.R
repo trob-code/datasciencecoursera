247ad7961b7b4ecb20f7d00e27d8c2b8d3dfcd18
trob-code <thomasr@gmail.com>
2016-10-31 01:09:40 -0700

    Cleaning up corr file to remove comments

--- 2_rprogramming/week2/corr.R
+++ 2_rprogramming/week2/corr.R
@@ -1,11 +1,5 @@
 corr <- function(directory, threshold = 0)
 {
-    # files <- file.path(directory, paste0(sprintf("%03d", id), ".csv"))
-    # tables <- lapply(files, read.csv)
-    # alldata <- do.call(rbind, tables)
-    # 
-    # mean(alldata[[pollutant]], na.rm = TRUE)
-    # 
     obssize <- complete(directory)
     thresholdIds <- obssize[obssize$nobs > threshold, 1]
     
